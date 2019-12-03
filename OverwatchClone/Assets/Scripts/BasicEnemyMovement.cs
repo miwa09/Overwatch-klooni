941c5081a3917461045f3c5395e009d0e5d45c79
Viljami Perheentupa <pviljo@gmail.com>
2019-12-04 00:30:08 +0200

    pientä navmesh paskaa

---
+++ OverwatchClone/Assets/Scripts/BasicEnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemyMovement : MonoBehaviour
+{
+    Vector3 origPoint;
+    public Transform waypoint;
+    NavMeshAgent ai;
+    bool reached = false;
+    
+    void Start()
+    {
+        origPoint = new Vector3(transform.position.x, 0, transform.position.z);
+        ai = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!reached) {
+            ai.SetDestination(waypoint.position);
+        }
+        if (Vector3.Distance(transform.position, waypoint.position) < 1.5f) {
+            reached = true;
+        }
+        if (reached) {
+            ai.SetDestination(origPoint);
+        }
+        if (Vector3.Distance(transform.position, origPoint) < 1.5f) {
+            reached = false;
+        }
+    }
+}
